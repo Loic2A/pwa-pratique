b883321f8b435591193e08f0c2782fc1544920a3
Loic2A <lomedori.15@gmail.com>
2020-09-24 14:17:17 +0200

    Add screen

--- sw.js
+++ sw.js
@@ -1,1 +1,14 @@
 console.log("Hello depuis le service worker");
+
+self.addEventListener('install', (evt) => {
+    console.log(`sw installé à ${new Date().toLocaleTimeString()}`);
+});
+
+self.addEventListener('activate', (evt) => {
+    console.log(`sw activé à ${new Date().toLocaleTimeString()}`);    
+});
+
+self.addEventListener('fetch', (evt) => {
+    console.log('sw intercepte la requête suivante via fetch', evt);
+    console.log('url interceptée', evt.request.url);
+});
