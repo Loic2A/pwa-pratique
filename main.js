aa9b9d3556e45771b6f7cae4ca887e757b54c435
Loic2A <lomedori.15@gmail.com>
2020-09-24 13:39:22 +0200

    Add main et package

--- main.js
+++ main.js
@@ -6,13 +6,18 @@ function loadTechnologies(technos) {
         .then(response => {
             response.json()
                 .then(technos => {
-                    const allTechnos = technos.map(t => `<div><b>${t.name}</b> ${t.description}  <a href="${t.url}">site de ${t.name}</a> </div>`)
-                            .join('');
-            
-                    technosDiv.innerHTML = allTechnos; 
-                });
-        })
+                    const allTechnos = technos.map(t => `	
+                    <div class="card">	
+                        <div class="card-body">	
+                        <h5 class="card-title">${t.name}</h5>	
+                        <p class="card-text">${t.description}</p>	
+                        <a href="${t.url}" class="card-link">site de ${t.name}</a>	
+                        </div>	
+                    </div>`)	
+                            .join('');	
+                    technosDiv.innerHTML = allTechnos; 	
+                });	
+        })	
         .catch(console.error);
 }
-
 loadTechnologies(technos);
